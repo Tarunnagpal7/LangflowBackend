c4745c42d28e9d682ef5eb1114af36225320fd59
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-01-05 15:52:41 +0530

    Debugging CORS error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const frontendURL = process.env.FRONTEND_URL
 app.use(express.json());
 app.use(cors(
   {
-    origin:"*",
+    origin:frontendURL,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true,
