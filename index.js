6dd0e4a3248d37521b5061a4ea099f934dcd7882
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-01-05 14:34:52 +0530

    Debugging CORS error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// server.js
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -11,7 +11,14 @@ const frontendURL = process.env.FRONTEND_URL
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:"*",
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials:true,
+  }
+));
 
 
 
